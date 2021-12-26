d59cb96def0b979b2a77be37a58af2fe47d31fb1
19189-GiraMuhammadNurIcharisma <1910631170189@student.unsika.ac.id>
2021-12-26 22:44:19 +0700

    a

--- sistempakar.py
+++ sistempakar.py
@@ -9,7 +9,7 @@ daftarGejala = ['demam','batuk','kehilangan indera perasa','sesak nafas','sulit 
                'kehilangan atau perubahan indera pengecapan atau penciuman','kelelahan','keringan di malam hari','tenggorokan gatal','sakit kepala','hilangnya indera penciuman',
                'sakit tenggorokan','nyeri sendi','hilangnya indera pengecapan','pegal-pegal']
 daftarVarian = ['alpha','beta','gamma','delta','lambda','kappa','eta','mu','omicron']
-gejala = 0
+
 
 def checkGejala():
     if request.form.get('pilihan') == 'ya':
@@ -42,91 +42,138 @@ def welcome():
 def result():
    if request.method == 'POST':
       #=============================================================Varian Alpha 
-      if session['logs'] == 0 and checkGejala():
+      if checkGejala():
          if session['gejalaPasien'] == 0: # Gejala 1
             session['gejalaPasien'] = 1
-            session['logs'] = 0
             return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 1:
+         if session['gejalaPasien'] == 1:
               session['gejalaPasien'] = 2
-              session['logs'] = 0
               return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 2:
+         if session['gejalaPasien'] == 2:
               session['gejalaPasien'] = 3
-              session['logs'] = 0
               return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 3:
+         if session['gejalaPasien'] == 3:
               session['gejalaPasien'] = 4
-              session['logs'] = 0
               return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 4:
+         if session['gejalaPasien'] == 4:
               session['gejalaPasien'] = 5
-              session['logs'] = 0
               return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 5:
+         if session['gejalaPasien'] == 5:
               session['gejalaPasien'] = 6
-              session['logs'] = 0
               return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 6:
+         if session['gejalaPasien'] == 6:
               session['gejalaPasien'] = 7
-              session['logs'] = 0
               return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 7:
+         if session['gejalaPasien'] == 7:
               session['gejalaPasien'] = 8
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 8:
+              session['gejalaPasien'] = 9
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 9:
+              session['gejalaPasien'] = 10
               session['logs'] = 0
               return redirect(url_for('diagnosa'))
-         elif session['gejalaPasien'] == 8 and session['logs']==0:
-              terjangkitVarian = daftarVarian[0]
-              return render_template("result.html", terjangkitVarian = terjangkitVarian, awal = url_for('index'))
+         if session['gejalaPasien'] == 10:
+              session['gejalaPasien'] = 11
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 11:
+              session['gejalaPasien'] = 12
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 12:
+              session['gejalaPasien'] = 13
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 13:
+              session['gejalaPasien'] = 14
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 14:
+              session['gejalaPasien'] = 15
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 15:
+              session['gejalaPasien'] = 16
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 16:
+              session['gejalaPasien'] = 17
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 17:
+              session['gejalaPasien'] = 18
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 18:
+              session['gejalaPasien'] = 19
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 19:
+              session['gejalaPasien'] = 20
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 20:
+              session['gejalaPasien'] = 21
+              session['logs'] = 0
+              return redirect(url_for('diagnosa'))
+         if session['gejalaPasien'] == 21:
+              session['gejalaPasien'] = 22
+              session['logs'] = 0
+              return redirect(url_for('result'))
+      if session['gejalaPasien'] == [0,1,2,3,4,5,6,7]:
+         terjangkitVarian = daftarVarian[0]
+         return render_template("result.html", terjangkitVarian = terjangkitVarian, awal = url_for('index'))
       #=============================================================Varian Beta
-      else :
-          if session['gejalaPasien'] == 1 and session['logs']==0:
-             session['gejalaPasien'] = 2
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 2:
-             session['gejalaPasien'] = 3
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 3:
-             session['gejalaPasien'] = 4
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 4:
-             session['gejalaPasien'] = 5
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 5:
-             session['gejalaPasien'] = 6
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 6:
-             session['gejalaPasien'] = 7
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 7:
-             session['gejalaPasien'] = 8
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 8:
-             session['gejalaPasien'] = 9
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          if session['gejalaPasien'] == 9:
-             session['gejalaPasien'] = 10
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          if session['gejalaPasien'] == 10:
-             session['gejalaPasien'] = 25
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          if session['gejalaPasien'] == 25:
-             session['gejalaPasien'] = 26
-             session['logs'] = 1
-             return redirect(url_for('diagnosa'))
-          elif session['gejalaPasien'] == 26 and session['logs'] == 1:
-               terjangkitVarian = daftarVarian[1]
-               return render_template("result.html", terjangkitVarian=terjangkitVarian, awal = url_for('index'))
+      # else :
+      #     if session['gejalaPasien'] == 1 and session['logs']==0:
+      #        session['gejalaPasien'] = 2
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 2:
+      #        session['gejalaPasien'] = 3
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 3:
+      #        session['gejalaPasien'] = 4
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 4:
+      #        session['gejalaPasien'] = 5
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 5:
+      #        session['gejalaPasien'] = 6
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 6:
+      #        session['gejalaPasien'] = 7
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 7:
+      #        session['gejalaPasien'] = 8
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 8:
+      #        session['gejalaPasien'] = 9
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     if session['gejalaPasien'] == 9:
+      #        session['gejalaPasien'] = 10
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     if session['gejalaPasien'] == 10:
+      #        session['gejalaPasien'] = 25
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     if session['gejalaPasien'] == 25:
+      #        session['gejalaPasien'] = 26
+      #        session['logs'] = 1
+      #        return redirect(url_for('diagnosa'))
+      #     elif session['gejalaPasien'] == 26 and session['logs'] == 1:
+      #          terjangkitVarian = daftarVarian[1]
+      #          return render_template("result.html", terjangkitVarian=terjangkitVarian, awal = url_for('index'))
      #==============================================================Logs 2
      
 @app.route('/diagnosa',methods = ['POST', 'GET'])
